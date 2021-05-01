c5392dcd5b64d8640c5d78770295f6abaa03e0f4
Benoît Valle <benoit.valleea@hotmail.fr>
2021-05-01 16:14:24 +0200

    feat init: spring project structure

---
+++ src/main/java/fr/esgi/grp9/uparserbackend/UparserBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.grp9.uparserbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UparserBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UparserBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/esgi/grp9/uparserbackend/UparserBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.grp9.uparserbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UparserBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
