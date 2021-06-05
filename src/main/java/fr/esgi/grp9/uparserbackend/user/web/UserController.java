5e2a10727305f31b8f201b8a37d7a8c061509872
Juliien <julien1105@outlook.fr>
2021-06-05 18:37:48 +0200

    fix: fix spring import

--- src/main/java/fr/esgi/grp9/uparserbackend/user/web/UserController.java
+++ src/main/java/fr/esgi/grp9/uparserbackend/user/web/UserController.java
@@ -4,7 +4,11 @@ import fr.esgi.grp9.uparserbackend.user.domain.User;
 import fr.esgi.grp9.uparserbackend.user.domain.UserServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
