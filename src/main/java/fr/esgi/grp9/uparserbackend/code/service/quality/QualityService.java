4e9330a5250f6debe1e75a2ce13c6432a02445fe
valle <benoit.valleea@hotmail.fr>
2021-07-25 18:41:32 +0200

    fix(import): import arrays

--- src/main/java/fr/esgi/grp9/uparserbackend/code/service/quality/QualityService.java
+++ src/main/java/fr/esgi/grp9/uparserbackend/code/service/quality/QualityService.java
@@ -10,10 +10,7 @@ import org.yaml.snakeyaml.util.ArrayUtils;
 import javax.xml.bind.DatatypeConverter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class QualityService implements IQualityService {
