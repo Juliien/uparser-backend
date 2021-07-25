300d972ad526a8b1f96fa1844dd815102a48e820
valle <benoit.valleea@hotmail.fr>
2021-07-25 03:17:23 +0200

    feat(refacto-kafka): add kafka props to resources and rework

--- src/main/java/fr/esgi/grp9/uparserbackend/kafka/service/IKafkaService.java
+++ src/main/java/fr/esgi/grp9/uparserbackend/kafka/service/IKafkaService.java
@@ -2,20 +2,14 @@ package fr.esgi.grp9.uparserbackend.kafka.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import fr.esgi.grp9.uparserbackend.kafka.domain.KafkaTransaction;
-import fr.esgi.grp9.uparserbackend.run.domain.Run;
 import fr.esgi.grp9.uparserbackend.run.domain.RunRaw;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 public interface IKafkaService {
-    Properties propertiesProvider(String action, String groupId);
-    ProducerRecord<String, KafkaTransaction> createProducerRecord(KafkaTransaction kafkaTransaction);
-    Producer<String, KafkaTransaction> createKafkaProducer(String groupId);
-    KafkaConsumer<String, String> createKafkaConsumer(String groupId);
+    void sendProducerRecord(KafkaTransaction kafkaTransaction) throws ExecutionException, InterruptedException, TimeoutException;
     RunRaw seekForRunnerResults(String runId) throws JsonProcessingException;
+    Properties propertiesProvider();
 }
