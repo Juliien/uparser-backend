f755927e8daef8ef5f9f74f59cf23610c88503af
valle <benoit.valleea@hotmail.fr>
2021-07-25 03:20:40 +0200

    feat(refacto): add getRunsByUserId and getRunsByCodeId to interface

--- src/main/java/fr/esgi/grp9/uparserbackend/run/service/IRunService.java
+++ src/main/java/fr/esgi/grp9/uparserbackend/run/service/IRunService.java
@@ -1,7 +1,6 @@
 package fr.esgi.grp9.uparserbackend.run.service;
 
 import fr.esgi.grp9.uparserbackend.run.domain.Run;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,5 +9,7 @@ public interface IRunService {
     Run createRun(Run run);
     Optional<Run> findRunById(String id);
     List<Run> getRuns();
+    Optional<List<Run>> getRunsByUserId(String userId);
+    Optional<List<Run>> getRunsByCodeId(String codeId);
     void deleteRunById(String id);
 }
