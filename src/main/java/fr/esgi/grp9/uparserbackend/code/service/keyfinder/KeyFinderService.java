7452fe6fa62c99222b3826acb8127ce0009f1b5e
valle <benoit.valleea@hotmail.fr>
2021-07-26 21:56:14 +0200

    feat(key-parser): add formattedCode, arrayOfSingleWords, arrayOfLinesOfSingleWords, arrayOfVariables, arrayOfParams, arrayOfFunctionNames

--- src/main/java/fr/esgi/grp9/uparserbackend/code/service/keyfinder/KeyFinderService.java
+++ src/main/java/fr/esgi/grp9/uparserbackend/code/service/keyfinder/KeyFinderService.java
@@ -1,28 +1,31 @@
 package fr.esgi.grp9.uparserbackend.code.service.keyfinder;
 
 import fr.esgi.grp9.uparserbackend.code.domain.Code;
-import org.apache.tomcat.util.buf.StringUtils;
 
 import java.util.*;
 
 public class KeyFinderService {
 
     private final Code code;
-    private final List<String> arrayOfSingleWords;
 
-    public KeyFinderService(Code code) {
+    private String formattedCode;
+    private List<String> arrayOfSingleWords = new ArrayList<>();
+    private List<List<String>> arrayOfLinesOfSingleWords = new ArrayList<>();
+
+    private List<String> arrayOfVariables = new ArrayList<>();
+    private List<String> arrayOfParams = new ArrayList<>();
+    private List<String> arrayOfFunctionNames = new ArrayList<>();
+
+    public KeyFinderService(Code code) throws Exception {
         this.code = code;
-        this.arrayOfSingleWords = getFinalListOfWordsFromCode();
-    }
 
-    public static void main(String[] args) {
-        Code code = Code.builder()
-                .language("python")
-                .extensionEnd("csv")
-                .extensionEnd("json")
-                .codeEncoded("IyBQeXRob24gcHJvZ3JhbSB0byBkaXNwbGF5IHRoZSBGaWJvbmFjY2kgc2VxdWVuY2UKZGVmIHJlY3VyX2ZpYm8obik6CiAgICAgICAgaWYgbiA8PSAxOgogICAgICAgIHJldHVybiBuCmVsc2U6CiAgICAgICByZXR1cm4ocmVjdXJfZmlibyhuLTEpICsgcmVjdXJfZmlibyhuLTIpKQoKbnRlcm1zID0gMTAKCiMgY2hlY2sgaWYgdGhlIG51bWJlciBvZiB0ZXJtcyBpcyB2YWxpZAppZiBudGVybXMgPD0gMDoKICAgIHByaW50KCJQbGVzZSBlbnRlciBhIHBvc2l0aXZlIGludGVnZXIiKQplbHNlOgogICAgcHJpbnQoIkZpYm9uYWNjaSBzZXF1ZW5jZToiKQogICAgZm9yIGkgaW4gcmFuZ2UobnRlcm1zKToKICAgICAgICBwcmludChyZWN1cl9maWJvKGkpKQog")
-                .build();
-//        new KeyFinderService(code).getFinalListOfWordsFromCode();
+        initFormattedCode();
+        initArrayOfSingleWords();
+        initArrayOfLinesOfSingleWords();
+        initVariablesFromArrayOfSingleWord();
+        initParamsFromArrayOfSingleWord();
+        initFunctionNameFromArrayOfSingleWord();
+        deleteDuplicationFromLists();
     }
 
     private String decodeCode(String code) {
@@ -30,53 +33,80 @@ public class KeyFinderService {
         return new String(decodedBytes);
     }
 
-    private List<String> findFunctionNameFromArrayOfSingleWord(List<String> strings){
-        List<String> arrayOfFunctionNames = new ArrayList<>();
-
-        for (int start = 0; start < strings.size(); start++) {
-            if (strings.get(start).contains("def")){
-                arrayOfFunctionNames.add(strings.get(start+1));
-            }
-        }
-        return arrayOfFunctionNames;
+    private void initFormattedCode(){
+        String begin = decodeCode(this.code.getCodeEncoded());
+        String spaced = reformatStringsSpaces(begin);
+        String formatted = reformatStringsEscapedChars(spaced);
+        this.formattedCode = formatted;
     }
 
-    private List<String> findVariablesFromArrayOfSingleWord(List<String> strings) {
-        List<String> arrayOfVariables = new ArrayList<>();
+    private void initArrayOfLinesOfSingleWords(){
+        List<List<String>> lines = new ArrayList<>();
+        List<String> tempList = new ArrayList<>();
+        List<String> strings = this.arrayOfSingleWords;
 
-        for (int start = 0; start < strings.size(); start++) {
-            if (strings.get(start).contains("=")){
-                if (!strings.get(start-1).contains("=") && !strings.get(start+1).contains("=")
-                        && !strings.get(start-1).contains(">") && !strings.get(start-1).contains("<")
-                        && !strings.get(start-1).contains("!")) {
-                    arrayOfVariables.add(strings.get(start - 1).trim());
+        for (String str: strings){
+            if (!str.equals("\\n")){
+                tempList.add(str);
+            } else {
+                lines.add(tempList);
+                tempList = new ArrayList<>();
+            }
+        }
+        this.arrayOfLinesOfSingleWords =  reformatLinesImportLess(lines);
+    }
+
+    private void initArrayOfSingleWords() throws Exception {
+        List<String> tempList = Arrays.asList(this.formattedCode.split("\\s"));
+        List<String> specificSpaceTrimmedList = trimSpecificSpacesFromArray(tempList);
+        List<String> printValueLessList = reformatStringsPrint(specificSpaceTrimmedList, "PRINT_DATA");
+        this.arrayOfSingleWords = printValueLessList;
+    }
+
+    private void initVariablesFromArrayOfSingleWord() {
+        for (int start = 0; start < this.arrayOfSingleWords.size(); start++) {
+            if (this.arrayOfSingleWords.get(start).contains("=")){
+                if (!this.arrayOfSingleWords.get(start-1).contains("=") && !this.arrayOfSingleWords.get(start+1).contains("=")
+                        && !this.arrayOfSingleWords.get(start-1).contains(">") && !this.arrayOfSingleWords.get(start-1).contains("<")
+                        && !this.arrayOfSingleWords.get(start-1).contains("!")) {
+                    this.arrayOfVariables.add(this.arrayOfSingleWords.get(start - 1).trim());
                 }
             }
         }
-        return arrayOfVariables;
     }
 
-    private List<String> findParamsFromArrayOfSingleWord(List<String> strings) {
-        List<String> arrayOfParam = new ArrayList<>();
-
-        for (int start = 0; start < strings.size(); start++) {
-            if (strings.get(start).contains("def")){
+    private void initParamsFromArrayOfSingleWord() {
+        for (int start = 0; start < this.arrayOfSingleWords.size(); start++) {
+            if (this.arrayOfSingleWords.get(start).contains("def")){
                 start = start + 3;
                 boolean shouldContinue = true;
                 while (shouldContinue){
-                    if (strings.get(start).contains(")")){
+                    if (this.arrayOfSingleWords.get(start).contains(")")){
                         shouldContinue = false;
-                    } else if (!strings.get(start).contains(",")){
-                        arrayOfParam.add(strings.get(start));
+                    } else if (!this.arrayOfSingleWords.get(start).contains(",")){
+                        this.arrayOfParams.add(this.arrayOfSingleWords.get(start));
                     }
                     start++;
                 }
             }
         }
-        return arrayOfParam;
     }
 
-    public List<String> deleteDuplicationFromList(List<String> strings){
+    private void initFunctionNameFromArrayOfSingleWord(){
+        for (int start = 0; start < this.arrayOfSingleWords.size(); start++) {
+            if (this.arrayOfSingleWords.get(start).contains("def")){
+                this.arrayOfFunctionNames.add(this.arrayOfSingleWords.get(start+1));
+            }
+        }
+    }
+
+    private void deleteDuplicationFromLists() {
+        this.arrayOfVariables = deleteDuplicationFromAList(this.arrayOfVariables);
+        this.arrayOfParams = deleteDuplicationFromAList(this.arrayOfParams);
+        this.arrayOfFunctionNames = deleteDuplicationFromAList(this.arrayOfFunctionNames);
+    }
+
+    private List<String> deleteDuplicationFromAList(List<String> strings){
         List<String> arrayOfStringDuplicationLess = new ArrayList<>();
         for (String str: strings) {
             if (!arrayOfStringDuplicationLess.contains(str)){
@@ -86,7 +116,7 @@ public class KeyFinderService {
         return arrayOfStringDuplicationLess;
     }
 
-    private String reformatString(String decode) {
+    private String reformatStringsSpaces(String decode) {
         List<String> keyChars = Arrays.asList("(", ")", "=", ",", "\"");
         for (String keyChar: keyChars) {
             boolean shouldContinue = true;
@@ -101,147 +131,124 @@ public class KeyFinderService {
                     shouldContinue = false;
                 }
             }
+            decode = decode.replace(keyChar, " " + keyChar + " ");
         }
-        return decode
-                .replace("(", " ( ")
-                .replace(")"," ) ")
-                .replace("="," = ")
-                .replace(","," , ")
-                .replace("\""," \" ");
+        return decode;
     }
 
+    private String reformatStringsEscapedChars(String string){
+        Hashtable<String, String> my_dict = new Hashtable<>();
+        my_dict.put("\t", "t");
+        my_dict.put("\n", "n");
+        my_dict.put("\b", "b");
+        my_dict.put("\r", "r");
+        my_dict.put("\f", "f");
 
-    public List<String> getFinalListOfWordsFromCode() {
-        String decode = decodeCode(this.code.getCodeEncoded());
-        String str = reformatString(decode);
-        return Arrays.asList(str.split(" "));
-
-//        List<String> initialCodeArray = new ArrayList<>(Arrays.asList(decodeCode(this.code.getCodeEncoded()).split(" ")));
-//
-//        for (String possibleMultipleWords: initialCodeArray) {
-//
-//            //test si potentiel function
-//            //si c'est le cas passer le string dans une fonction qui va le decouper et ajouter son contenu au tableau
-//            //sinon tester presence variable puis decouper et ajouter si c'est le cas
-//            if (possibleMultipleWords.contains("def") && !possibleMultipleWords.contains("\\:")) {
-//                //faire la fonction que recup jusqu'a :
-////            } else if (){
-////
-//            }
-//            //contient def mais pas de :){
-////            faire la fonction qui recupère tous jusqu'au colon
-//
-//            if (possibleMultipleWords.contains("def") || possibleMultipleWords.contains("\\(")
-//                    || possibleMultipleWords.contains("\\)")  || possibleMultipleWords.contains("\\:")){
-//                //ici faire la fonction qui cherche les params
-//            } else if (possibleMultipleWords.contains("=")) {
-//                //ici faire la fonction qui cherche les variables
-//            } else {
-//                //ajouter le mot complet au tableau
-//            }
-//
-//            if(findVariableFromString(possibleMultipleWords).isPresent()){
-//                arrayOfSingleWords.addAll(findVariableFromString(possibleMultipleWords).get());
-//            }
-//
-//        }
-//
-//        for (int i = 0; i < initialCodeArray.size(); i++) {
-//            //si le bloque de fonction n'est pas complet
-//            if (initialCodeArray.get(i).contains("def") && !initialCodeArray.get(i).contains("\\:")) {
-//                //ajoute les element separer dans le tableau
-//                arrayOfSingleWords.addAll(getParamsFromArray(getEverythingTillColon(initialCodeArray, i)));
-//                //met l'iterator après le bloque si contient le colon pour ne pas l'ajouter dans le tableau
-//                i = setIteratorToColonPosition(initialCodeArray, i);
-//            } else if (initialCodeArray.get(i).contains("def") && initialCodeArray.get(i).contains("\\:")){
-////                arrayOfSingleWords.addAll()
-//            } else {
-//                arrayOfSingleWords.add(initialCodeArray.get(i));
-//            }
-//        }
+        for (Map.Entry<String, String> set:
+                my_dict.entrySet()) {
+            string = string.replace(set.getKey()," \\" + set.getValue() + " ");
+        }
+        return string;
     }
 
+    public List<String> reformatStringsPrint(List<String> strings, String replaceWith) throws Exception {
+        String print_data = replaceWith;
+        List<String> finalList = new ArrayList<>();
+        for (int i = 0; i < strings.size(); i++) {
+            if(strings.get(i).equals("print")){
+                try {
+                    if(strings.get(i+1).equals("(")){
+                        finalList.add(strings.get(i));
+                        if(!strings.get(i+2).equals(")")){
+                            i++;
+                            int depth = 0;
+                            boolean shouldContinue = true;
+                            while (shouldContinue) {
+                                if(strings.get(i).equals("(")){
+                                    if (depth > 0){
+                                        finalList.add(print_data);
+                                    } else {
+                                        finalList.add(strings.get(i));
+                                    }
+                                    depth++;
+                                } else if (strings.get(i).equals(")")){
+                                    depth--;
+                                    if (depth > 0){
+                                        finalList.add(print_data);
+                                    } else {
+                                        finalList.add(strings.get(i));
+                                    }
+                                } else {
+                                    finalList.add(print_data);
+                                }
+                                i++;
+                                if (depth == 0){
+                                    shouldContinue = false;
+                                }
+                            }
+                        }
+                    } else {
+                        throw new Exception("Print syntax Error");
+                    }
+                } catch (Exception e) {
+                    throw new Exception(e.getMessage());
+                }
+            } else {
+                finalList.add(strings.get(i));
+            }
+        }
+        return finalList;
+    }
 
-//    private List<String> findParamFromString(String str){
-//        List<String> res = new ArrayList<>();
-//        try{
-//            str = str.split("\\(")[1];
-//            str = str.split("\\)")[0];
-//            for (String arg: str.split(",")) {
-//                res.add(arg.trim());
-//            }
-//        } catch (Exception exception) {
-//
+    private List<String> trimSpecificSpacesFromArray(List<String> strings){
+        List<String> finalList = new ArrayList<>();
+        finalList.add(strings.get(0));
+        for (int i = 1; i < strings.size()-1; i++) {
+            if (!((!strings.get(i-1).equals("") && !strings.get(i+1).equals("")) && strings.get(i).equals(""))){
+                finalList.add(strings.get(i));
+            }
+        }
+        return finalList;
+    }
+
+    private void debugDisplayOfArray(List<String> strings){
+        for (int i = 1; i < strings.size()-1; i++) {
+            System.out.println( i + " : " + strings.get(i));
+        }
+    }
+
+    private List<List<String>> reformatLinesImportLess(List<List<String>> lines) {
+        List<List<String>> finalLines = new ArrayList<>();
+        for (List<String> line: lines){
+            if (!line.contains("import") || !line.contains("from")){
+                finalLines.add(line);
+            }
+        }
+        return finalLines;
+    }
+
+//    public String getArrayOfSingleWordsWithEscapeSequences(String string) {
+//        List<String> escapeSequences = Arrays.asList("\t", "\n", "\b", "\r", "\f");
+//        for (String escapeSequence: escapeSequences) {
+//            string = string.replace(escapeSequence, "\\" + escapeSequence);
 //        }
-//        return res;
+//        return string;
 //    }
 
-//    private List<String> getParamsFromArray(List<String> strings) {
-//        String all = StringUtils.join(strings);
-//        return findParamFromString(all);
-//    }
+    public static void main(String[] args) throws Exception {
+        Code code = Code.builder()
+                .language("python")
+                .extensionEnd("csv")
+                .extensionEnd("json")
+                .codeEncoded("IyBQeXRob24gcHJvZ3JhbSB0byBkaXNwbGF5IHRoZSBGaWJvbmFjY2kgc2VxdWVuY2UKZGVmIHJlY3VyX2ZpYm8obik6CiAgICAgICAgaWYgbiA8PSAxOgogICAgICAgIHJldHVybiBuCmVsc2U6CiAgICAgICByZXR1cm4ocmVjdXJfZmlibyhuLTEpICsgcmVjdXJfZmlibyhuLTIpKQoKbnRlcm1zID0gMTAKCiMgY2hlY2sgaWYgdGhlIG51bWJlciBvZiB0ZXJtcyBpcyB2YWxpZAppZiBudGVybXMgPD0gMDoKICAgIHByaW50KCJQbGVzZSBlbnRlciBhIHBvc2l0aXZlIGludGVnZXIiKQplbHNlOgogICAgcHJpbnQoIkZpYm9uYWNjaSBzZXF1ZW5jZToiKQogICAgZm9yIGkgaW4gcmFuZ2UobnRlcm1zKToKICAgICAgICBwcmludChyZWN1cl9maWJvKGkpKQog")
+                .build();
 
-//    private int setIteratorToColonPosition(List<String> strings, int start) {
-//        while(true){
-//            if (strings.get(start+1).contains(":")){
-//                break;
-//            }
-//            start++;
-//        }
-//        return start;
-//    }
-
-//    private List<String> getEverythingTillColon(List<String> strings, int start) {
-//        List<String> res = new ArrayList<>();
-//        while(true){
-//            if (strings.get(start+1).contains(":")){
-//                break;
-//            }
-//            res.add(strings.get(start));
-//            start++;
-//        }
-//        return res;
-
-//        strings.get(i).split("\\(")[1];
-//        if(strings.get(i).contains("\\:") || strings.get(i).contains("\\)")) {
-//
-//            //ici recuperer les arguments
-//        } else if (strings.get(i).contains("\\(")) {
-//            //faire i
-//        }
-//
-//
-//            if(){
-//                if(!strings.get(i).contains("\\(")){
-//                    i++;
-//                }
-//            } else {
-//                //ici on a pas la parenthèse fermante donc
-//            }
-//        } else {
-//            //ici on a tous le bloque donc chercher tous les args
-//        }
-//
-//        while(true){
-//
-//        }
-//    }
-
-//    private Optional<List<String>> findVariableFromString(String str){
-//        Optional<List<String>> optVariablePlusEqualPlusValue = Optional.of(new ArrayList<>());
-//        List<String> variablePlusEqualPlusValue = new ArrayList<>();
-//        try {
-//            str = str.trim();
-//            String[] elements = str.split("=");
-//            for (String element : elements) {
-//                optVariablePlusEqualPlusValue.get().add(element);
-//            }
-//        } catch (Exception exception) {
-//
-//        }
-//        return optVariablePlusEqualPlusValue;
-//    }
-
-
-
+        KeyFinderService keyFinderService = new KeyFinderService(code);
+        System.out.println("keyFinderService.formattedCode = " + keyFinderService.formattedCode);
+        System.out.println("keyFinderService.arrayOfSingleWords = " + keyFinderService.arrayOfSingleWords);
+        System.out.println("keyFinderService.arrayOfLinesOfSingleWords = " + keyFinderService.arrayOfLinesOfSingleWords);
+        System.out.println("keyFinderService.arrayOfVariables = " + keyFinderService.arrayOfVariables);
+        System.out.println("keyFinderService.arrayOfParams = " + keyFinderService.arrayOfParams);
+        System.out.println("keyFinderService.arrayOfFunctionNames = " + keyFinderService.arrayOfFunctionNames);
+    }
 }
