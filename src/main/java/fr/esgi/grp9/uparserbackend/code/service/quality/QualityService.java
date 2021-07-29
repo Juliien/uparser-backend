688bba5cc92e28e4330d813e3b899aeb4fa2c1b9
valle <benoit.valleea@hotmail.fr>
2021-07-29 02:56:46 +0200

    feat(plagiarism): improve exactness of the isCodePlagiarism function

--- src/main/java/fr/esgi/grp9/uparserbackend/code/service/quality/QualityService.java
+++ src/main/java/fr/esgi/grp9/uparserbackend/code/service/quality/QualityService.java
@@ -5,6 +5,8 @@ import fr.esgi.grp9.uparserbackend.code.domain.Code;
 import fr.esgi.grp9.uparserbackend.code.domain.CodeRepository;
 import fr.esgi.grp9.uparserbackend.code.domain.quality.Grade;
 import fr.esgi.grp9.uparserbackend.code.domain.quality.GradeRepository;
+import fr.esgi.grp9.uparserbackend.code.domain.quality.HashCode;
+import fr.esgi.grp9.uparserbackend.code.domain.quality.HashCodeRepository;
 import fr.esgi.grp9.uparserbackend.code.service.keyfinder.KeyFinderService;
 import fr.esgi.grp9.uparserbackend.code.domain.parser.ParserResponse;
 import fr.esgi.grp9.uparserbackend.code.service.parser.PythonParser;
@@ -14,46 +16,92 @@ import org.springframework.stereotype.Service;
 import javax.xml.bind.DatatypeConverter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class QualityService implements IQualityService {
     private final CodeRepository codeQualityRepository;
     private final GradeRepository gradeRepository;
+    private final HashCodeRepository hashCodeRepository;
     private final PythonParser pythonParser = new PythonParser();
 
-    public QualityService(CodeRepository codeQualityRepository, GradeRepository gradeRepository) {
+    public QualityService(CodeRepository codeQualityRepository, GradeRepository gradeRepository, HashCodeRepository hashCodeRepository) {
         this.codeQualityRepository = codeQualityRepository;
         this.gradeRepository = gradeRepository;
+        this.hashCodeRepository = hashCodeRepository;
     }
 
     @Override
     public Code isCodePlagiarism(Code code) throws NoSuchAlgorithmException {
-        // decode base64
-        String decodeCode = this.decodeString(code.getCodeEncoded());
-        // trim code
-        // decodementer
-//        String codeTrim = this.prepareCode(decodeCode);
-        // hash code
-        String hash = this.createMD5Hash(decodeCode);
+        KeyFinderService keyFinderService = new KeyFinderService(code);
 
-        List<Code> codeExist = this.checkIfCodeExist(hash);
-        if(codeExist.size() > 0) {
+        List<String> linesHash = new ArrayList<>();
+        Hashtable<String, String> funHash = new Hashtable<>();
+
+        for (List<String> line: keyFinderService.getArrayOfLinesOfSingleWordsForPlagiarism()){
+            linesHash.add(createMD5Hash(line.toString()));
+        }
+
+        if (linesHash.size() == 0){
+            code.setPlagiarism(false);
+            return code;
+        }
+
+        for (Map.Entry<String, List<List<String>>> set:
+                keyFinderService.getArrayOfFunctionBodyByNames().entrySet()) {
+            funHash.put(set.getKey(), createMD5Hash(set.getValue().toString()));
+        }
+
+        HashCode hashCode = HashCode.builder()
+                .codeId(code.getId())
+                .linesHash(linesHash)
+                .funHash(funHash)
+                .build();
+
+        List<HashCode> listOfHashCode = this.hashCodeRepository.findAll();
+
+        if (listOfHashCode.size() == 0) {
+            code.setPlagiarism(false);
+            hashCodeRepository.save(hashCode);
+            return code;
+        }
+
+        Integer numberOfSameLines = 0;
+        Integer numberOfSameFunctions = 0;
+
+        for (HashCode fetchedHashCode: listOfHashCode) {
+            for (String hashLine : hashCode.getLinesHash()) {
+                for (String fetchedHashLine: fetchedHashCode.getLinesHash()){
+                    if (hashLine.equals(fetchedHashLine)){
+                        numberOfSameLines++;
+                        break;
+                    }
+                }
+            }
+
+            for (Map.Entry<String, String> funSet: hashCode.getFunHash().entrySet()) {
+                for (Map.Entry<String, String> fetchedFunSet: fetchedHashCode.getFunHash().entrySet()) {
+                    if (fetchedFunSet.getValue().equals(funSet.getValue())) {
+                        numberOfSameFunctions++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        float linesPlagiarismRate = 100 - ((((float) linesHash.size() - (float) numberOfSameLines)/(float) linesHash.size())) * 100;
+        float funPlagiarismRate = 100 - ((((float) funHash.size() - (float) numberOfSameFunctions)/(float) funHash.size())) * 100;
+
+        // if plagiarism is detected
+        // 20.0 is the % limit of code that could be considered as plagarism without setting the isPlagarism to true
+        if (linesPlagiarismRate < 20.0 || funPlagiarismRate < 20.0) {
+            code.setPlagiarism(false);
+            hashCodeRepository.save(hashCode);
+        } else {
             code.setPlagiarism(true);
         }
-        code.setHash(hash);
-        return code;
-    }
-    private List<Code> checkIfCodeExist(String hash) {
-        return this.codeQualityRepository.findAllByHash(hash);
-    }
 
-    private void parseCode(String code) {
-        // count lignes of code
-        String[] lines = code.split("\r\n|\r|\n");
-        System.out.println(lines.length);
+        return code;
     }
 
     @Override
@@ -75,9 +123,6 @@ public class QualityService implements IQualityService {
         return ParserResponse.builder().result(_result).build();
     }
 
-
-    ////////////////////////////////////////////////////////////////
-
     private String decodeString(String s) {
         byte[] decodedBytes = Base64.getDecoder().decode(s);
         return new String(decodedBytes);
@@ -92,14 +137,12 @@ public class QualityService implements IQualityService {
 
     @Override
     public Code testCodeQuality(Code code) {
-        //TODO faire les traitements pour le grade
-        //change le type de retour par un object grade avec 10 boolean
-        //zero si le code ne compile
-        //if isValid == false renvoie grade 0
 
         Grade grade = new Grade(new KeyFinderService(code));
 
         String gradeId = this.gradeRepository.save(grade).getId();
+
+        code.setCodeMark(grade.getAverage());
 
         code.setGradeId(gradeId);
 
